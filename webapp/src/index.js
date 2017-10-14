6da7dc20e009f15508f4aceab698f2d0bf2fbbc3
Salem <salmahmo@amazon.com>
2017-10-14 15:41:35 -0400

    dirty provider hack

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -8,15 +8,27 @@ import _ from 'underscore';
 
 document.addEventListener('DOMContentLoaded', () => {
   // initialize web3
+  let provider;
   if (typeof window.web3 !== 'undefined') {
+    provider = window.web3.currentProvider;
     window.web3 = new Web3(window.web3.currentProvider);
   } else {
+    provider = new Web3.providers.HttpProvider('http://localhost:8545');
     // set the provider you want from Web3.providers
-    window.web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
+    window.web3 = new Web3(provider);
   }
 
-  _.each(contracts, contract => contract.setProvider(window.web3.currentProvider));
-
+  _.each(contracts, contract => {
+    contract.setProvider(provider);
+    if (typeof contract.currentProvider.sendAsync !== "function") {
+      contract.currentProvider.sendAsync = function() {
+        return contract.currentProvider.send.apply(
+          contract.currentProvider, arguments
+        );
+      };
+    }
+  });
+  
   ReactDOM.render(<App/>, document.getElementById('root'));
 });
 
--- webapp/src/pages/GovernmentPage.jsx
+++ webapp/src/pages/GovernmentPage.jsx
@@ -1,64 +1,83 @@
 import React, { Component } from 'react';
+import { Government } from '../util/contracts';
 
 export default class GovernmentPage extends Component {
   static propTypes = {};
 
+  componentDidMount() {
+    Government.deployed()
+      .then(
+        government => console.log(government)
+      );
+  }
+
   render() {
     return (
-	  <div className="container">
-	  <h2>Candidates for Citizenship</h2>
-	  <p>The following individuals have paid funds in accordance with US tax law into CitEthZen's escrow contract.  These funds will be released to the US Government upon naturalization.</p>            
-	  <table className="table">
-	    <thead>
-	      <tr>
-	        <th>Age</th>
-	        <th>Occupation</th>
-	        <th>Education</th>
-	        <th>2014 Contribution</th>
-	        <th>2015 Contribution</th>
-	        <th>2016 Contribution</th>
-	        <th>Total Contribution</th>
-	        <th>Accpetance</th>
-	        <th>Collection</th>
-	      </tr>
-	    </thead>
-	    <tbody>
-	      <tr>
-	        <td>25</td>
-	        <td>Doctor</td>
-	        <td>B.A. in Computer Science</td>
-	        <td>$1,500</td>
-	        <td>$2,000</td>
-	        <td>$1,000</td>
-	        <td>$4,500</td>
-	        <td>Accepted</td>
-	        <td><button bsStyle="primary">Collect</button></td>
-	      </tr>
-	      <tr>
-	        <td>25</td>
-	        <td>Lawyer</td>
-	        <td>Bachelor of Laws</td>
-			<td>$1,500</td>
-	        <td>$2,000</td>
-	        <td>$1,000</td>
-	        <td>$4,500</td>
-	       	<td><button bsStyle="primary">Accpet</button></td>
-	        <td><button bsStyle="primary">Collect</button></td>
-	      </tr>
-	      <tr>
-	        <td>25</td>
-	        <td>Farmer</td>
-	        <td>Masters in Agriculture</td>
-	        <td>$1,500</td>
-	        <td>$2,000</td>
-	        <td>$1,000</td>
-	        <td>$4,500</td>
-	        <td><button bsStyle="primary">Accpet</button></td>
-	        <td><button bsStyle="primary">Collect</button></td>
-	      </tr>
-	    </tbody>
-	  </table>
-	</div>
+      <div className="container">
+        <h2>Candidates for Citizenship</h2>
+        <p>The following individuals have paid funds in accordance with US tax law into CitEthZen's escrow contract.
+          These funds will be released to the US Government upon naturalization.</p>
+        <table className="table">
+          <thead>
+          <tr>
+            <th>Age</th>
+            <th>Occupation</th>
+            <th>Education</th>
+            <th>2014 Contribution</th>
+            <th>2015 Contribution</th>
+            <th>2016 Contribution</th>
+            <th>Total Contribution</th>
+            <th>Accpetance</th>
+            <th>Collection</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr>
+            <td>25</td>
+            <td>Doctor</td>
+            <td>B.A. in Computer Science</td>
+            <td>$1,500</td>
+            <td>$2,000</td>
+            <td>$1,000</td>
+            <td>$4,500</td>
+            <td>Accepted</td>
+            <td>
+              <button bsStyle="primary">Collect</button>
+            </td>
+          </tr>
+          <tr>
+            <td>25</td>
+            <td>Lawyer</td>
+            <td>Bachelor of Laws</td>
+            <td>$1,500</td>
+            <td>$2,000</td>
+            <td>$1,000</td>
+            <td>$4,500</td>
+            <td>
+              <button bsStyle="primary">Accpet</button>
+            </td>
+            <td>
+              <button bsStyle="primary">Collect</button>
+            </td>
+          </tr>
+          <tr>
+            <td>25</td>
+            <td>Farmer</td>
+            <td>Masters in Agriculture</td>
+            <td>$1,500</td>
+            <td>$2,000</td>
+            <td>$1,000</td>
+            <td>$4,500</td>
+            <td>
+              <button bsStyle="primary">Accpet</button>
+            </td>
+            <td>
+              <button bsStyle="primary">Collect</button>
+            </td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
     );
   }
 }
