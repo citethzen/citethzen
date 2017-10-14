0031165e94370b1dbc03101080789dde9d5aea94
Salem <salmahmo@amazon.com>
2017-10-14 16:48:25 -0400

    updates to the government immigrant stuff

--- webapp/src/pages/ImmigrantPage.jsx
+++ webapp/src/pages/ImmigrantPage.jsx
@@ -3,29 +3,29 @@ import React, { Component } from 'react';
 export default class ImmigrantPage extends Component {
   static propTypes = {};
 
-   NewGreeting(props) {
+  NewGreeting(props) {
     return (
-			<form>
-			<div className="form-group">
-				<label for="input1">Age</label>
-				<select class="form-control" id="input1">
-				  <option>18-25</option>
-				  <option>26-35</option>
-				  <option>36-45</option>
-				  <option>46-60</option>
-				  <option>61+</option>
-				</select>
-			</div>
-			<div className="form-group">
-				<label for="input2">Occupation</label>
-				<input className="form-control" id="input2"/>
-			</div>
-			<div className="form-group">
-				<label for="input3">Education</label>
-				<input className="form-control" id="input3"/>
-			</div>
-			<button class="btn btn-primary">Submit</button>
-			</form>
+		<form>
+		<div className="form-group">
+			<label >Age</label>
+			<select className="form-control" id="input1">
+			  <option>18-25</option>
+			  <option>26-35</option>
+			  <option>36-45</option>
+			  <option>46-60</option>
+			  <option>61+</option>
+			</select>
+		</div>
+		<div className="form-group">
+			<label htmlFor="input2">Occupation</label>
+			<input className="form-control" id="input2"/>
+		</div>
+		<div className="form-group">
+			<label htmlFor="input3">Education</label>
+			<input className="form-control" id="input3"/>
+		</div>
+		<button className="btn btn-primary">Submit</button>
+		</form>
     );
   }
 
@@ -62,7 +62,7 @@ export default class ImmigrantPage extends Component {
   	}
 
 	render() {
-		
+
 		return (
 			false ? this.NewGreeting(this.props) : this.RegisteredGreeting(this.props)
 		)
