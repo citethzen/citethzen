4128b2ab6fef30b8e61baf4c8018911505fac00f
Gillian Chu <gillianchu@Gillians-MacBook-Pro.local>
2017-10-14 14:33:57 -0400

    moved wallet file into contracts

---
+++ contracts/Wallet.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.4.16;
+import "./ERC20.sol";
+import "./Immigrant.sol";
+import "./Government.sol";
+
+contract Wallet{
+  address public owner;
+
+  function Wallet(address _owner){
+    owner = _owner;
+  }
+
+  modifier onlyOwner {
+    require(msg.sender == owner);
+    _;
+  }
+
+  function withdrawETH(address withdrawTo) onlyOwner returns (bool) {
+    withdrawTo.transfer(this.balance);
+    return true;
+  }
+
+  function withdrawTokens(address tokenAddress, address withdrawTo) onlyOwner {
+    ERC20 token = ERC20(tokenAddress);
+    token.transfer(government, token.balanceOf(this));
+  }
+}
